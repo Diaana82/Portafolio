9ee9038de5bb3eb149717a1adb3753f6b43f3752
Diana Cristina Patiño Ramirez <patinoramirezdianacristina82@gmail.com>
2022-08-20 22:46:20 -0500

    I defined the project boxes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="nav-bar">
+    <div>
+      <img src="" alt="logo">
+    </div>
+
+    <div class="nav-bar__right">
+      <a href="">Say hello</a>
+    </div>
   </nav>
   <router-view/>
+  <footer>
+    <div>
+      <img src="" alt="logo">
+    </div>
+  <div class="main-footer__phrase">
+    <p>"If you think good architecture is expensive, try bad architecture."</p>
+  </div>
+
+  <div class="main-footer__social-media">
+    <a href="#"><Icon icon="el:github-text" /> github</a>
+    <a href="#"><Icon icon="eva:linkedin-fill" /> linkedin</a>
+  </div>
+  <div class="grupo-2">
+    <small>&copy; 2022 <b> Diaana82</b></small>
+  </div>
+  </footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: auto;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+</style>
+<style>
+.nav-bar{
+  background-color: blueviolet;
+}
+footer{
+  background-color: crimson;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <header class="home">
+      <section class="txt-header">
+        <h1>¡Hola mi nombre es Diana!</h1>
+        <h3>Soy Desarroladora web</h3>
+      </section>
+    </header>
+    <section class="container-introduction">
+        <div>
+          <h1 class="">sobre mi</h1>
+          <p class="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus libero vitae, enim eum
+            iste eos. In voluptate, ad aut culpa omnis accusantium assumenda consequatur, voluptatum
+            voluptatibus pariatur officia maxime similique!
+          </p>
+          </div>
+    </section>
+    <div>
+      <section class="project">
+              <div class="item-1">
+                <h1 class="info-title">MI PORYECTO</h1>
+                <p class="info-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing
+                   elit. Dolorem illo voluptas iure obcaecati cumque. Culpa libero ratione,
+                    molestias eligendi vero dolorem nostrum incidunt officia, quae illo
+                     voluptatibus, praesentium id? Et.</p>
+              </div>
+              <div class="item-2">
+                <img class="imagen" src="" alt="Logo">
+              </div>
+        </section>
+        <section class="tecno">
+          <h1>Tecnologías</h1>
+          <div>
+            <img src="" alt="vue">
+            <img src="" alt="js">
+            <img src="" alt="html">
+            <img src="" alt="css">
+          </div>
+        </section>
+        <section class="buttonn">
+          <input type="button" value="¡Contáctame!">
+        </section>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
+
+<style>
+.home{
+  width: 100%;
+  height: 450px;
+  position: relative;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: darkgrey;
+}
+.container-introduction{
+  background-color: hotpink;
+}
+.project{
+  background-color: lawngreen;
+}
+.item-1{
+  grid-area: a;
+}
+.item-2{
+  grid-area: b;
+  justify-content: center;
+}
+.project{
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  grid-template-rows: auto auto;
+  grid-template-areas:
+    "a a"
+    "b b";
+}
+@media (min-width: 600px) {
+  .project {
+    grid-template-areas:
+    "a  b";
+  }
+}
+.tecno{
+  background-color: aqua;
+}
+.buttonn{
+  background-color: lightgreen;
+}
+</style>
